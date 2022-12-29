0e5e722ffacdf2da781f04c7cf8a68255ad96435
Riccardo <riccardo@Riccardos-MacBook-Pro-2.fritz.box>
2022-12-29 22:52:08 +0100

    fix deplaoyment

--- components/slider/Slide.js
+++ components/slider/Slide.js
@@ -1,42 +1,42 @@
-import { forwardRef } from "react";
+// import { forwardRef } from "react";
 
-// const Tab = forwardRef((props, ref) => {
-//   const { label } = props;
-//   return (
-//     <li {...props} ref={ref}>
-//       {label}
-//     </li>
-//   );
-// });
-// const Slide = forwardRef((props, ref) => {
-const Slide = forwardRef(
-  (
-    {
-      // key,
-      // innerRef,
-      // slideDataIndex,
-      // setSlideIndex,
-      title,
-      className,
-    },
-    ref
-  ) => {
-    return (
-      <div
-        ref={ref}
-        // key={key}
-        className={`
-          flex
-          items-center
-          justify-items-center
-          w-[400px] h-[300px]
-          ${className}
-        `}
-      >
-        <h1 className="text-xl font-bold">{title}</h1>
-      </div>
-    );
-  }
-);
+// // const Tab = forwardRef((props, ref) => {
+// //   const { label } = props;
+// //   return (
+// //     <li {...props} ref={ref}>
+// //       {label}
+// //     </li>
+// //   );
+// // });
+// // const Slide = forwardRef((props, ref) => {
+// const Slide = forwardRef(
+//   (
+//     {
+//       // key,
+//       // innerRef,
+//       // slideDataIndex,
+//       // setSlideIndex,
+//       title,
+//       className,
+//     },
+//     ref
+//   ) => {
+//     return (
+//       <div
+//         ref={ref}
+//         // key={key}
+//         className={`
+//           flex
+//           items-center
+//           justify-items-center
+//           w-[400px] h-[300px]
+//           ${className}
+//         `}
+//       >
+//         <h1 className="text-xl font-bold">{title}</h1>
+//       </div>
+//     );
+//   }
+// );
 
-export default Slide;
+// export default Slide;
