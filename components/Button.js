723f2a41d0ec7034fba6be6c531d40092fe332df
Riccardo <riccardo@Riccardos-MBP-2.fritz.box>
2022-12-30 16:11:41 +0100

    add subscribe() function to index

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,21 @@
 import Link from "next/link";
 
-export function Button(props) {
-  const { children } = props;
-
-  return (
-    <Link
-      className="
-      inline-block
-      whitespace-nowrap
+export function Button({
+  className,
+  children,
+  href,
+  type = "button",
+  onClick,
+  disabled,
+}) {
+  const CSSclasses = `
+    inline-block
+    whitespace-nowrap
     text-white
     font-bold
     tracking-wide
     rounded-lg
-    px-4 py-3 mr-2 mb-2
+    px-4 py-3
     bg-gray-900
     hover:bg-gray-800
     0dark:bg-gray-50
@@ -20,10 +23,33 @@ export function Button(props) {
     focus:outline-none
     focus:ring-4
     focus:ring-blue-300
-    0dark:focus:ring-blue-500"
-      href="#"
-    >
-      {children}
-    </Link>
-  );
+    0dark:focus:ring-blue-500
+    ${" " + className}
+  `;
+
+  if (href) {
+    return (
+      <Link className={CSSclasses} href={href}>
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className={CSSclasses}
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  // return (
+  //   <button className={CSSclasses}>
+  //     {href ? <Link href="#">{children}</Link> : <Link href="#">{children}</Link>}
+  //     <Link href="#">{children}</Link>
+  //   </button>
+  // );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,66 @@
+import React, { useRef, useState } from "react";
+
+import { setCookie, hasCookie } from "cookies-next";
+
 import Head from "next/head";
 import Image from "next/image";
 
 import Page from "../layouts/page";
+import { Button } from "../components/Button";
 
 export default function Home() {
-  // const [data, setData] = useState(null);
-  // const [isLoading, setLoading] = useState(false);
+  // 1. Create a reference to the input so we can fetch/clear it's value.
+  const inputEl = useRef(null);
+  // 2. Hold a message in state to handle the response from our API.
+  const [message, setMessage] = useState("");
+  // const [hasEmail, setHasEmail] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
+  const subscribe = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
 
-  //   // temp
-  //   // setLoading(false);
-  //   // setData(res200);
+    // if ((inputEl.current.value = null)) {
+    //   setMessage("You need to add a valid email.");
+    //   return;
+    // }
 
-  //   fetch("http://20.113.19.4:80/api/v1/service/aks-d80a/score", {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: `Bearer ${token}`,
-  //     },
-  //     body: JSON.stringify(deets),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       setLoading(false);
-  //     })
-  //     .catch((err) => console.error(err));
-  // }, []);
+    // 3. Send a request to our API with the user's email address.
+    const res = await fetch("/api/subscribe", {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
 
-  // if (isLoading) return <p>Loading...</p>;
-  // if (!data) return <p>No profile data</p>;
+    const { error } = await res.json();
+
+    if (error) {
+      // 4. If there was an error, update the message in state.
+
+      // 4b. If there was already an email, but uncaught, setCookie.
+      if (res.status == 400) {
+        setCookie("hasEmail", "true", { maxAge: 60 * 60 * 24 * 365 });
+      }
+
+      setMessage(error);
+
+      return;
+    }
+
+    // 5. Clear the input value and show a success message.
+    setMessage("Success! 🎉 You will hear from us soon.");
+
+    setCookie("hasEmail", "true", { maxAge: 60 * 60 * 24 * 365 });
+
+    setTimeout(() => {
+      window.open(`https://tally.so/r/wvXL5d?email=${inputEl.current.value}`);
+    }, 500);
+
+    // inputEl.current.value = "";
+  };
 
   return (
     <div className="bg-gray-100">
@@ -62,17 +92,11 @@ export default function Home() {
             </h1>
 
             <p className="mb-8 text-lg xl:w-10/12">
-              {/* Boost your content creation by 10x and grow your business. */}
               Boost your content creation by 10x, be on top of your insights and
               grow your business.
-              {/* Your AI companion to create content for your audience 10x faster
-              and grow your business. */}
-              {/* Create relevant content for your audience 10x faster and grow your */}
-              {/* Scale your content creation, be on top of your insights and grow
-              your business. */}
             </p>
 
-            <a
+            {/* <a
               className="
              inline-block
               whitespace-nowrap
@@ -98,7 +122,36 @@ export default function Home() {
               rel="noreferrer"
             >
               Get early access
-            </a>
+            </a> */}
+            <form className="flex flex-col gap-2" onSubmit={subscribe}>
+              <p>
+                <strong>Want to learn more?</strong>
+                <small className="block">
+                  Be one of the first to hear about us.
+                </small>
+              </p>
+              <label className="hidden font-bold mb-2" htmlFor="email-input">
+                Your email address
+              </label>
+              <div className="flex flex-col lg:flex-row gap-2">
+                <input
+                  className="
+                  py-2 px-4
+                  border border-gray-300
+                  bg-white
+                  rounded-lg
+                "
+                  id="email-input"
+                  name="email"
+                  type="email"
+                  ref={inputEl}
+                  required
+                  placeholder="email@provider.com"
+                />
+                <Button type="submit">Get early access</Button>
+              </div>
+              <p className="mt-2 text-sm">{message ? message : null}</p>
+            </form>
           </div>
           <div className="lg:pl-8 lg:basis-3/5 mb-12 lg:mb-0">
             <Image
